56bf3ef971b8b2c7b1472aa0ae5fd5b66063d07e
ANOA00778 <chartier.nick+github@gmail.com>
2026-06-26 23:54:35 +0200

    Create haier_ir_remote.h

---
+++ components/haier_ir_remote/haier_ir_remote.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/components/climate/climate.h"
+
+namespace esphome {
+namespace haier_ir_remote {
+
+class HaierIRRemoteClimate : public climate::Climate,
+                             public Component {
+ public:
+  void setup() override;
+  void loop() override;
+
+ protected:
+  climate::ClimateTraits traits() override;
+  void control(const climate::ClimateCall &call) override;
+};
+
+}  // namespace haier_ir_remote
+}  // namespace esphome
